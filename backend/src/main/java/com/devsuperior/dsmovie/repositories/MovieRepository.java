dda0a096a255a62dbd2aa7ec0b6540f0aa873c1d
Ronicley Angelo <ronicley8@hotmail.com>
2022-05-15 00:18:47 -0300

    Find movies

--- backend/src/main/java/com/devsuperior/dsmovie/repositories/MovieRepository.java
+++ backend/src/main/java/com/devsuperior/dsmovie/repositories/MovieRepository.java
@@ -1,14 +1,11 @@
 package com.devsuperior.dsmovie.repositories;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
-import org.springframework.data.domain.Page;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmovie.entities.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-
-	Page<Movie> findAll(Pageable pageable);
 	
 }
 
